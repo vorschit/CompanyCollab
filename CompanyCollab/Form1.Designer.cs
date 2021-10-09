e90094cbdb1102304af702cec0ef8e4c8d6e8d79
MUHAMMAD ADIIB <muhammad.adiib@s.unikl.edu.my>
2021-10-09 14:24:54 +0800

    UserInterface: - Top header panel - Close button - background color - text color

--- CompanyCollab/Form1.Designer.cs
+++ CompanyCollab/Form1.Designer.cs
@@ -39,12 +39,15 @@ namespace CompanyCollab
             this.label4 = new System.Windows.Forms.Label();
             this.lblConnSuccessFail = new System.Windows.Forms.Label();
             this.dataGridViewSyarikat = new System.Windows.Forms.DataGridView();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.panel_header = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewSyarikat)).BeginInit();
+            this.panel_header.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(486, 75);
+            this.button1.Location = new System.Drawing.Point(494, 156);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(149, 39);
             this.button1.TabIndex = 0;
@@ -54,21 +57,27 @@ namespace CompanyCollab
             // 
             // txtAgensi
             // 
-            this.txtAgensi.Location = new System.Drawing.Point(116, 10);
+            this.txtAgensi.BackColor = System.Drawing.Color.Gray;
+            this.txtAgensi.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtAgensi.ForeColor = System.Drawing.SystemColors.Window;
+            this.txtAgensi.Location = new System.Drawing.Point(124, 91);
             this.txtAgensi.Name = "txtAgensi";
             this.txtAgensi.Size = new System.Drawing.Size(267, 20);
             this.txtAgensi.TabIndex = 1;
             // 
             // txtAktiviti
             // 
-            this.txtAktiviti.Location = new System.Drawing.Point(116, 75);
+            this.txtAktiviti.BackColor = System.Drawing.Color.Gray;
+            this.txtAktiviti.ForeColor = System.Drawing.Color.White;
+            this.txtAktiviti.Location = new System.Drawing.Point(124, 156);
             this.txtAktiviti.Name = "txtAktiviti";
             this.txtAktiviti.Size = new System.Drawing.Size(267, 20);
             this.txtAktiviti.TabIndex = 2;
             // 
             // txtSkop
             // 
-            this.txtSkop.Location = new System.Drawing.Point(116, 134);
+            this.txtSkop.BackColor = System.Drawing.Color.Gray;
+            this.txtSkop.Location = new System.Drawing.Point(124, 215);
             this.txtSkop.Multiline = true;
             this.txtSkop.Name = "txtSkop";
             this.txtSkop.Size = new System.Drawing.Size(267, 86);
@@ -77,7 +86,8 @@ namespace CompanyCollab
             // lblConnStatus
             // 
             this.lblConnStatus.AutoSize = true;
-            this.lblConnStatus.Location = new System.Drawing.Point(483, 160);
+            this.lblConnStatus.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblConnStatus.Location = new System.Drawing.Point(491, 241);
             this.lblConnStatus.Name = "lblConnStatus";
             this.lblConnStatus.Size = new System.Drawing.Size(100, 13);
             this.lblConnStatus.TabIndex = 4;
@@ -87,7 +97,8 @@ namespace CompanyCollab
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(15, 16);
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(23, 97);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(88, 13);
             this.label2.TabIndex = 5;
@@ -97,7 +108,8 @@ namespace CompanyCollab
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(18, 81);
+            this.label3.ForeColor = System.Drawing.SystemColors.Control;
+            this.label3.Location = new System.Drawing.Point(26, 162);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(73, 13);
             this.label3.TabIndex = 6;
@@ -107,7 +119,8 @@ namespace CompanyCollab
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(18, 140);
+            this.label4.ForeColor = System.Drawing.SystemColors.Control;
+            this.label4.Location = new System.Drawing.Point(26, 221);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(84, 13);
             this.label4.TabIndex = 7;
@@ -116,7 +129,7 @@ namespace CompanyCollab
             // lblConnSuccessFail
             // 
             this.lblConnSuccessFail.AutoSize = true;
-            this.lblConnSuccessFail.Location = new System.Drawing.Point(599, 160);
+            this.lblConnSuccessFail.Location = new System.Drawing.Point(607, 241);
             this.lblConnSuccessFail.Name = "lblConnSuccessFail";
             this.lblConnSuccessFail.Size = new System.Drawing.Size(35, 13);
             this.lblConnSuccessFail.TabIndex = 8;
@@ -125,17 +138,43 @@ namespace CompanyCollab
             // dataGridViewSyarikat
             // 
             this.dataGridViewSyarikat.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridViewSyarikat.Location = new System.Drawing.Point(18, 279);
+            this.dataGridViewSyarikat.Location = new System.Drawing.Point(37, 330);
             this.dataGridViewSyarikat.Name = "dataGridViewSyarikat";
-            this.dataGridViewSyarikat.Size = new System.Drawing.Size(577, 416);
+            this.dataGridViewSyarikat.Size = new System.Drawing.Size(885, 361);
             this.dataGridViewSyarikat.TabIndex = 9;
             this.dataGridViewSyarikat.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewSyarikat_CellContentClick);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(920, 3);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 10;
+            this.btnClose.Text = "Exit";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel_header
+            // 
+            this.panel_header.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panel_header.Controls.Add(this.btnClose);
+            this.panel_header.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel_header.Location = new System.Drawing.Point(0, 0);
+            this.panel_header.Name = "panel_header";
+            this.panel_header.Size = new System.Drawing.Size(998, 33);
+            this.panel_header.TabIndex = 11;
+            this.panel_header.Paint += new System.Windows.Forms.PaintEventHandler(this.panel_header_Paint);
+            this.panel_header.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel_header_MouseDown);
+            this.panel_header.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel_header_MouseMove);
+            this.panel_header.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel_header_MouseUp);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.ClientSize = new System.Drawing.Size(998, 721);
+            this.Controls.Add(this.panel_header);
             this.Controls.Add(this.dataGridViewSyarikat);
             this.Controls.Add(this.lblConnSuccessFail);
             this.Controls.Add(this.label2);
@@ -146,10 +185,12 @@ namespace CompanyCollab
             this.Controls.Add(this.txtAktiviti);
             this.Controls.Add(this.txtAgensi);
             this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewSyarikat)).EndInit();
+            this.panel_header.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -167,6 +208,8 @@ namespace CompanyCollab
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label lblConnSuccessFail;
         private System.Windows.Forms.DataGridView dataGridViewSyarikat;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Panel panel_header;
     }
 }
 
--- CompanyCollab/Form1.cs
+++ CompanyCollab/Form1.cs
@@ -16,6 +16,10 @@ namespace CompanyCollab
 {
     public partial class Form1 : Form
     {
+        // (declaration) drag header of application
+        bool drag = false;
+        Point start_point = new Point(0, 0);
+
         // connection to the firebase
         IFirebaseConfig config = new FirebaseConfig
         {
@@ -121,6 +125,38 @@ namespace CompanyCollab
             {
                 dataGridViewSyarikat.Rows.Add(item.Key, item.Value.Agensi, item.Value.Aktiviti, item.Value.Skop);
             }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void panel_header_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel_header_MouseDown(object sender, MouseEventArgs e)
+        {
+            //enable drag application
+            drag = true;
+            start_point = new Point(e.X, e.Y);
+
+        }
+
+        private void panel_header_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (drag)
+            {
+                Point p = PointToScreen(e.Location);
+                this.Location = new Point(p.X - start_point.X, p.Y - start_point.Y);
+            }
+        }
+
+        private void panel_header_MouseUp(object sender, MouseEventArgs e)
+        {
+            drag = false;
         }
     }
 }

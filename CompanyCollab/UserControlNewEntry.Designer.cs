ad611b7820f5e23033af68dd798ac4cea9c8285f
Muhammad Adiib <81273210+vorschit@users.noreply.github.com>
2021-10-10 16:27:04 +0800

    - change text forecolor for NewEntry textbox - change color button exit

--- CompanyCollab/UserControlNewEntry.Designer.cs
+++ CompanyCollab/UserControlNewEntry.Designer.cs
@@ -30,20 +30,20 @@ namespace CompanyCollab
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.txtSkop = new System.Windows.Forms.TextBox();
-            this.txtAgensi = new System.Windows.Forms.TextBox();
-            this.txtAktiviti = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSkop = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtAktiviti = new System.Windows.Forms.TextBox();
+            this.txtAgensi = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
@@ -55,84 +55,10 @@ namespace CompanyCollab
             this.panel1.Controls.Add(this.button1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(748, 688);
+            this.panel1.Size = new System.Drawing.Size(997, 847);
             this.panel1.TabIndex = 0;
-            // 
-            // txtSkop
-            // 
-            this.txtSkop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
-            this.txtSkop.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.txtSkop.Location = new System.Drawing.Point(129, 201);
-            this.txtSkop.Multiline = true;
-            this.txtSkop.Name = "txtSkop";
-            this.txtSkop.Size = new System.Drawing.Size(462, 111);
-            this.txtSkop.TabIndex = 11;
-            // 
-            // txtAgensi
-            // 
-            this.txtAgensi.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
-            this.txtAgensi.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.txtAgensi.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.txtAgensi.ForeColor = System.Drawing.SystemColors.Window;
-            this.txtAgensi.Location = new System.Drawing.Point(129, 3);
-            this.txtAgensi.Multiline = true;
-            this.txtAgensi.Name = "txtAgensi";
-            this.txtAgensi.Size = new System.Drawing.Size(462, 81);
-            this.txtAgensi.TabIndex = 9;
-            // 
-            // txtAktiviti
-            // 
-            this.txtAktiviti.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
-            this.txtAktiviti.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.txtAktiviti.ForeColor = System.Drawing.Color.White;
-            this.txtAktiviti.Location = new System.Drawing.Point(129, 90);
-            this.txtAktiviti.Multiline = true;
-            this.txtAktiviti.Name = "txtAktiviti";
-            this.txtAktiviti.Size = new System.Drawing.Size(462, 105);
-            this.txtAktiviti.TabIndex = 10;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.ForeColor = System.Drawing.SystemColors.Control;
-            this.label2.Location = new System.Drawing.Point(3, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(88, 13);
-            this.label2.TabIndex = 12;
-            this.label2.Text = "Agensi / Syarikat";
-            // 
-            // button1
-            // 
-            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(79)))), ((int)(((byte)(195)))), ((int)(((byte)(247)))));
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button1.Location = new System.Drawing.Point(63, 616);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(149, 39);
-            this.button1.TabIndex = 8;
-            this.button1.Text = "Add";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.ForeColor = System.Drawing.SystemColors.Control;
-            this.label3.Location = new System.Drawing.Point(3, 87);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(73, 13);
-            this.label3.TabIndex = 13;
-            this.label3.Text = "Aktiviti Agensi";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.ForeColor = System.Drawing.SystemColors.Control;
-            this.label4.Location = new System.Drawing.Point(3, 198);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(84, 13);
-            this.label4.TabIndex = 14;
-            this.label4.Text = "Skop Kerjasama";
             // 
             // tableLayoutPanel1
             // 
@@ -151,25 +77,97 @@ namespace CompanyCollab
             this.tableLayoutPanel1.Controls.Add(this.textBox1, 1, 3);
             this.tableLayoutPanel1.Controls.Add(this.textBox2, 1, 4);
             this.tableLayoutPanel1.Controls.Add(this.textBox3, 1, 5);
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(28, 23);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(37, 28);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 6;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 43.84615F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 56.15385F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 117F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 61F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 66F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 119F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(594, 562);
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 144F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 75F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 81F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 146F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(792, 692);
             this.tableLayoutPanel1.TabIndex = 15;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(4, 0);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 17);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Agensi / Syarikat";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.Control;
+            this.label3.Location = new System.Drawing.Point(4, 107);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 17);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Aktiviti Agensi";
+            // 
+            // txtSkop
+            // 
+            this.txtSkop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
+            this.txtSkop.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtSkop.ForeColor = System.Drawing.Color.Black;
+            this.txtSkop.Location = new System.Drawing.Point(171, 249);
+            this.txtSkop.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtSkop.Multiline = true;
+            this.txtSkop.Name = "txtSkop";
+            this.txtSkop.Size = new System.Drawing.Size(617, 136);
+            this.txtSkop.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.SystemColors.Control;
+            this.label4.Location = new System.Drawing.Point(4, 245);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(111, 17);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Skop Kerjasama";
+            // 
+            // txtAktiviti
+            // 
+            this.txtAktiviti.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
+            this.txtAktiviti.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtAktiviti.ForeColor = System.Drawing.Color.Black;
+            this.txtAktiviti.Location = new System.Drawing.Point(171, 111);
+            this.txtAktiviti.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtAktiviti.Multiline = true;
+            this.txtAktiviti.Name = "txtAktiviti";
+            this.txtAktiviti.Size = new System.Drawing.Size(617, 130);
+            this.txtAktiviti.TabIndex = 10;
+            // 
+            // txtAgensi
+            // 
+            this.txtAgensi.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
+            this.txtAgensi.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtAgensi.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtAgensi.ForeColor = System.Drawing.Color.Black;
+            this.txtAgensi.Location = new System.Drawing.Point(171, 4);
+            this.txtAgensi.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtAgensi.Multiline = true;
+            this.txtAgensi.Name = "txtAgensi";
+            this.txtAgensi.Size = new System.Drawing.Size(617, 99);
+            this.txtAgensi.TabIndex = 9;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.ForeColor = System.Drawing.SystemColors.Control;
-            this.label1.Location = new System.Drawing.Point(3, 315);
+            this.label1.Location = new System.Drawing.Point(4, 389);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.Size = new System.Drawing.Size(60, 17);
             this.label1.TabIndex = 15;
             this.label1.Text = "Tempoh";
             // 
@@ -177,9 +175,10 @@ namespace CompanyCollab
             // 
             this.label5.AutoSize = true;
             this.label5.ForeColor = System.Drawing.SystemColors.Control;
-            this.label5.Location = new System.Drawing.Point(3, 376);
+            this.label5.Location = new System.Drawing.Point(4, 464);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(105, 13);
+            this.label5.Size = new System.Drawing.Size(136, 17);
             this.label5.TabIndex = 16;
             this.label5.Text = "Kejayaan / Outcome";
             // 
@@ -187,9 +186,10 @@ namespace CompanyCollab
             // 
             this.label6.AutoSize = true;
             this.label6.ForeColor = System.Drawing.SystemColors.Control;
-            this.label6.Location = new System.Drawing.Point(3, 442);
+            this.label6.Location = new System.Drawing.Point(4, 545);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(37, 13);
+            this.label6.Size = new System.Drawing.Size(48, 17);
             this.label6.TabIndex = 17;
             this.label6.Text = "Status";
             // 
@@ -197,39 +197,59 @@ namespace CompanyCollab
             // 
             this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
             this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.textBox1.Location = new System.Drawing.Point(129, 318);
+            this.textBox1.ForeColor = System.Drawing.Color.Black;
+            this.textBox1.Location = new System.Drawing.Point(171, 393);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(462, 55);
+            this.textBox1.Size = new System.Drawing.Size(617, 67);
             this.textBox1.TabIndex = 18;
             // 
             // textBox2
             // 
             this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
             this.textBox2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.textBox2.Location = new System.Drawing.Point(129, 379);
+            this.textBox2.ForeColor = System.Drawing.Color.Black;
+            this.textBox2.Location = new System.Drawing.Point(171, 468);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(462, 60);
+            this.textBox2.Size = new System.Drawing.Size(617, 73);
             this.textBox2.TabIndex = 19;
             // 
             // textBox3
             // 
             this.textBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
             this.textBox3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.textBox3.Location = new System.Drawing.Point(129, 445);
+            this.textBox3.ForeColor = System.Drawing.Color.Black;
+            this.textBox3.Location = new System.Drawing.Point(171, 549);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox3.Multiline = true;
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(462, 114);
+            this.textBox3.Size = new System.Drawing.Size(617, 139);
             this.textBox3.TabIndex = 20;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(79)))), ((int)(((byte)(195)))), ((int)(((byte)(247)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Location = new System.Drawing.Point(84, 758);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(199, 48);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // UserControlNewEntry
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "UserControlNewEntry";
-            this.Size = new System.Drawing.Size(748, 688);
+            this.Size = new System.Drawing.Size(997, 847);
             this.panel1.ResumeLayout(false);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
